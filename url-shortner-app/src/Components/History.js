ff70cfbc9faba95178c7257b057011dd7e143bbe
Azhar Abbass <712020:e1e9d789-f43b-4751-8830-7245d6ac16ff@bots.bitbucket.org>
2023-05-05 15:44:27 +0500

    App is ready for user's testing.

---
+++ url-shortner-app/src/Components/History.js
@@ -0,0 +1,38 @@
+function History(){
+    var urlData = JSON.parse(localStorage.getItem('Urls') || '[]');
+    return(
+        <div className="historyDiv">
+        <h2 className="historyHeading">Short Urls:</h2>
+        <table className="historyTable">
+                <thead className='tablehead'>
+                    
+                        <tr>
+                            <th>Id</th>
+                            <th>Long Url</th>
+                            <th>Short Url</th>
+                            <th>expiryDate</th>
+                        </tr>
+                            
+                </thead>
+                <tbody>
+                {urlData.map((curUser)=>{
+                    const{id, longUrl, shortUrl, expiryDate}=curUser;
+                        return(
+                        <>
+                            <tr key={id}>
+                                <td>{id}</td>
+                                <td>{shortUrl}</td>
+                                <td>{longUrl}</td>
+                                <td>{expiryDate}</td>
+                            </tr>
+                            </>
+                    
+                        )}
+                    
+                        )}
+                    </tbody>
+            </table>
+        </div>
+    )
+}
+export default History;
---
+++ url-shortner-app/src/Components/HomePage.js
@@ -0,0 +1,88 @@
+import {useReducer} from "react"
+import {toast, ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+const InitailValue = {
+  id: 0,
+  longUrl: '',
+  shortUrl: '',
+  expiryDate: '',
+};
+const message = "Url is shortened successfully please go to hitory page to copy short url.";
+const message2 = "Something went wrong! Please try again with a valid url."
+function settingId(){
+  var oldArray = JSON.parse(localStorage.getItem("Urls") || "[]");
+  for(let i = 0; i < oldArray.length; i++){
+    if(i === (oldArray.length) -1){
+      var lastObject = oldArray[i];
+      return lastObject.id;
+    }
+  }
+}
+function reducer(state, action){
+  switch(action.type){
+    case 'changeValue':
+      return{...state, [action.field]:action.value};
+    default:
+      throw new Error();
+  }
+}
+function HomePage(){
+  const [state, dispatch] = useReducer(reducer, InitailValue);
+  const handleInputs = (event) => {
+    dispatch({
+      type: 'changeValue',
+      field : event.target.name,
+      value : event.target.value,
+    })
+  }
+    const onSubmit = (event) => {
+      var states = JSON.parse(localStorage.getItem('Urls') || "[]");
+      var previousId = settingId();
+      state.id = previousId + 1;
+      event.preventDefault();
+      try{
+        let urlOld = state.longUrl;
+        let num = Math.floor(Math.random()*4);
+        let num1 = num + 13;
+        let newUrl = urlOld.slice(0,num1) + '.com'
+        state.shortUrl = newUrl;
+        states.push(state);
+        localStorage.setItem("Urls", JSON.stringify(states));
+        toast.success(message, {position: toast.POSITION.BOTTOM_CENTER});
+      }
+      catch(e){
+        toast.error(message2, {position:toast.POSITION.BOTTOM_CENTER})
+      }
+      
+    }
+  
+    return(
+        <div className="homeDiv">
+            <h2 className="homeText" >Enter your url to short</h2>
+            <form className="inputForm" onSubmit={onSubmit}>
+
+         <input className="homeInput"
+           type="url"
+           name="longUrl"
+            value={state.longUrl}
+            onChange={handleInputs}
+           placeholder="Enter your url"
+         />
+        
+         <input className="homeInput"
+           type="date"
+           name="expiryDate"
+           value={state.expiryDate}
+           onChange={handleInputs}
+           placeholder="Enter expiry date"
+         />
+       
+       <button className="btn" type="submit" >Short</button>
+       <ToastContainer />
+
+
+     </form>
+        </div>
+    )
+}
+export default HomePage;
